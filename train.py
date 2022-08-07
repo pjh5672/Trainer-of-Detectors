fix:run NMS iou calculation debug

train.py
@@ -108,7 +108,7 @@ def execute_val(rank, world_size, config, dataloader, model, criterion, evaluato
             pred_yolo = torch.cat(predictions, dim=1)[idx].cpu().numpy()
             pred_yolo[:, :4] = clip_box_coordinates(bboxes=pred_yolo[:, :4]/config['INPUT_SIZE'])
             pred_yolo = filter_obj_score(prediction=pred_yolo, conf_threshold=config['MIN_SCORE_THRESH'])
-            pred_yolo = run_NMS_for_yolo(prediction=pred_yolo, iou_threshold=config['MIN_IOU_THRESH'], maxDets=config['MAX_DETS'])
+            pred_yolo = run_NMS_for_YOLO(prediction=pred_yolo, iou_threshold=config['MIN_IOU_THRESH'], multi_label=True, maxDets=config['MAX_DETS'])
             
             if len(pred_yolo) > 0:
                 detections.append((filename, pred_yolo, max_side))

utils/process.py
@@ -9,23 +9,24 @@ def filter_obj_score(prediction, conf_threshold=0.01):
     return np.concatenate([class_ids[:, np.newaxis], bboxes, conf_scores[:, np.newaxis]], axis=-1)
 
 
-def run_NMS_for_yolo(prediction, iou_threshold=0.5, maxDets=100):
+def run_NMS_for_YOLO(prediction, iou_threshold=0.5, multi_label=False, maxDets=100):
+    cls_ids = prediction[:, 0]
     bboxes = prediction[:, 1:5] * 100
     scores = prediction[:, 5]
 
-    if len(bboxes) == 0:
+    if len(prediction) == 0:
         return []
-        
+
     if bboxes.dtype.kind == "i":
         bboxes = bboxes.astype("float")
-        
+
     x1 = np.maximum(bboxes[:, 0] - bboxes[:, 2]/2, 0)
     y1 = np.maximum(bboxes[:, 1] - bboxes[:, 3]/2, 0)
     x2 = bboxes[:, 0] + bboxes[:, 2]/2
     y2 = bboxes[:, 1] + bboxes[:, 3]/2
     areas = (x2 - x1 + 1) * (y2 - y1 + 1)
     order = scores.argsort()[::-1][:maxDets]
-
+            
     pick = []
     while len(order) > 0:
         i = order[0]
@@ -33,7 +34,7 @@ def run_NMS_for_yolo(prediction, iou_threshold=0.5, maxDets=100):
 
         if len(order) == 1:
             break
-            
+
         xx1 = np.maximum(x1[i], x1[order[1:]])
         yy1 = np.maximum(y1[i], y1[order[1:]])
         xx2 = np.minimum(x2[i], x2[order[1:]])
@@ -42,7 +43,6 @@ def run_NMS_for_yolo(prediction, iou_threshold=0.5, maxDets=100):
         h = np.maximum(0, yy2 - yy1 + 1)
 
         overlap = (w * h)
-        ious = overlap / (areas[0] + areas[order[1:]] - overlap + 1e-8)
+        ious = overlap / (areas[i] + areas[order[1:]] - overlap + 1e-8)
         order = order[np.where(ious <= iou_threshold)[0] + 1]
-    
     return prediction[pick]