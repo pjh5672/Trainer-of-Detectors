fix:chnage BCELoss -> BCEWithLogitLoss due to stability in case of AMP computation

loss_function/yolov3.py
@@ -38,8 +38,7 @@ class YOLOv3_Loss():
         self.strides = [model.head.head_L.stride, model.head.head_M.stride, model.head.head_S.stride]
         self.num_anchors = len(self.anchors)
         self.mae_loss = nn.L1Loss(reduction='sum')
-        self.bce_loss = nn.BCELoss(reduction='sum')
-
+        self.bce_loss = nn.BCEWithLogitsLoss(reduction='sum')
 
 
     def __call__(self, predictions, targets):
@@ -55,13 +54,13 @@ class YOLOv3_Loss():
 
             prediction_each_scale = prediction_each_scale.view(self.batch_size, self.num_anchor_per_scale, self.grid_size, self.grid_size, -1)
             pred_box = prediction_each_scale[..., :4]
-            pred_obj = prediction_each_scale[..., 4]
-            pred_cls = prediction_each_scale[..., 5:]
+            pred_obj_logit = prediction_each_scale[..., 4]
+            pred_cls_logit = prediction_each_scale[..., 5:]
 
             pred_tx, pred_ty, pred_tw, pred_th = self.transfrom_batch_pred_loss_form(pred_box, anchor_each_scale, stride_each_scale)
             target_loss_forms = self.transform_batch_target_loss_form(targets, anchor_each_scale)
             b_obj_mask, b_noobj_mask, b_target_tx, b_target_ty, b_target_tw, b_target_th, b_target_cls = target_loss_forms
-            b_target_obj = b_obj_mask.float()
+            b_target_obj = b_obj_mask.clone()
             b_obj_mask = b_obj_mask.type(torch.BoolTensor)
             b_noobj_mask = b_noobj_mask.type(torch.BoolTensor)
 
@@ -69,9 +68,9 @@ class YOLOv3_Loss():
             loss_ty = self.mae_loss(pred_ty[b_obj_mask], b_target_ty[b_obj_mask])
             loss_tw = self.mae_loss(pred_tw[b_obj_mask], b_target_tw[b_obj_mask])
             loss_th = self.mae_loss(pred_th[b_obj_mask], b_target_th[b_obj_mask])
-            loss_obj = self.bce_loss(pred_obj[b_obj_mask], b_target_obj[b_obj_mask])
-            loss_noobj = self.bce_loss(pred_obj[b_noobj_mask], b_target_obj[b_noobj_mask])
-            loss_cls = self.bce_loss(pred_cls[b_obj_mask], b_target_cls[b_obj_mask])
+            loss_obj = self.bce_loss(pred_obj_logit[b_obj_mask], b_target_obj[b_obj_mask])
+            loss_noobj = self.bce_loss(pred_obj_logit[b_noobj_mask], b_target_obj[b_noobj_mask])
+            loss_cls = self.bce_loss(pred_cls_logit[b_obj_mask], b_target_cls[b_obj_mask])
 
             coord_loss += (loss_tx + loss_ty + loss_tw + loss_th)
             obj_loss += loss_obj
@@ -106,9 +105,9 @@ class YOLOv3_Loss():
 
     def build_target_mask(self, grid_ij, target_xy, target_wh, anchor_each_scale):
         obj_mask = torch.zeros(size=(self.num_anchor_per_scale, self.grid_size, self.grid_size), 
-                               device=self.device, dtype=torch.uint8)
+                               device=self.device, dtype=torch.float32)
         noobj_mask = torch.ones(size=(self.num_anchor_per_scale, self.grid_size, self.grid_size), 
-                                device=self.device, dtype=torch.uint8)
+                                device=self.device, dtype=torch.float32)
         iou_target_with_anchor = [get_IoU_target_with_anchor(target_wh.t(), anchor) for anchor in anchor_each_scale]
         iou_target_with_anchor = torch.stack(iou_target_with_anchor, dim=0)
         best_iou, best_anchor_index = iou_target_with_anchor.max(dim=0)
@@ -130,15 +129,15 @@ class YOLOv3_Loss():
                                 device=self.device, dtype=torch.float32)
         target_th = torch.zeros(size=(self.num_anchor_per_scale, self.grid_size, self.grid_size), 
                                 device=self.device, dtype=torch.float32)
-        target_cls = torch.zeros(size=(self.num_anchor_per_scale, self.grid_size, self.grid_size, self.num_classes), 
+        target_cls = torch.zeros(size=(self.num_anchor_per_scale, self.grid_size, self.grid_size, self.num_classes),
                                 device=self.device, dtype=torch.float32)
         target_c = target[:, 0].long()
         
         if -1 in target_c:
             obj_mask = torch.zeros(size=(self.num_anchor_per_scale, self.grid_size, self.grid_size), 
-                                   device=self.device, dtype=torch.uint8)
+                                   device=self.device, dtype=torch.float32)
             noobj_mask = torch.ones(size=(self.num_anchor_per_scale, self.grid_size, self.grid_size), 
-                                    device=self.device, dtype=torch.uint8)
+                                    device=self.device, dtype=torch.float32)
             return obj_mask, noobj_mask, target_tx, target_ty, target_tw, target_th, target_cls
 
         target_xy = target[:, 1:3] * self.grid_size

models/yolov3.py
@@ -15,18 +15,20 @@ class YOLOv3_Model(nn.Module):
 
         self.input_size = item['INPUT_SIZE']
         self.anchors = [x for x in item['ANCHORS'].values()]
+        self.num_attribute = 5 + num_classes
+        self.num_anchor_per_scale = len(self.anchors[0])
+        self.last_dim_channels = self.num_attribute * self.num_anchor_per_scale
         self.backbone = Darknet53_backbone(pretrained=pretrained)
-        self.fpn = YOLOv3_FPN()
+        self.fpn = YOLOv3_FPN(last_dim_channels=self.last_dim_channels)
         self.head = YOLOv3_head(input_size=self.input_size, 
                                 num_classes=num_classes, 
-                                anchors=self.anchors)
-
+                                anchors=self.anchors,
+                                num_anchor_per_scale=self.num_anchor_per_scale)
 
     def forward(self, x):
         features = self.backbone(x)
         features = self.fpn(features)
         predictions = self.head(features)
-
         return predictions
 
 

models/yolov3_modules/head.py
@@ -1,25 +1,18 @@
 import torch
 import torch.nn as nn
 
-from element import ConvLayer
 
 
 
 class DetectLayer(nn.Module):
-    def __init__(self, input_size, in_channels, num_classes, anchors, num_anchor_per_scale):
+    def __init__(self, input_size, num_classes, anchors, num_anchor_per_scale):
         super().__init__()
         self.input_size = input_size
         self.num_classes = num_classes
-        self.num_anchor_per_scale = num_anchor_per_scale
         self.num_attribute =  5 + num_classes
-        self.last_dim_channels = self.num_attribute * self.num_anchor_per_scale
+        self.num_anchor_per_scale = num_anchor_per_scale
         self.anchor_w = anchors[:, 0].view((1, self.num_anchor_per_scale, 1, 1))
         self.anchor_h = anchors[:, 1].view((1, self.num_anchor_per_scale, 1, 1))
-
-        self.conv = nn.Sequential(
-            ConvLayer(in_channels, in_channels*2, 3, stride=1, padding=1),
-            nn.Conv2d(in_channels*2, self.last_dim_channels, 1, stride=1, padding=0)
-        )
 
 
     def forward(self, x):
@@ -28,15 +21,12 @@ class DetectLayer(nn.Module):
         grid_size = x.shape[2]
         self.stride = self.input_size / grid_size
 
-        out = self.conv(x)
-        out = out.view(batch_size, self.num_anchor_per_scale, self.num_attribute, grid_size, grid_size)
+        out = x.view(batch_size, self.num_anchor_per_scale, self.num_attribute, grid_size, grid_size)
         out = out.permute(0, 1, 3, 4, 2).contiguous()
-
         grid_xy = self.compute_grid_offset(grid_size)
         pred_bboxes = self.transform_pred_coords(bboxes=out[..., :4], grid_xy=grid_xy)
-        pred_object = torch.sigmoid(out[..., 4])
-        pred_class = torch.sigmoid(out[..., 5:])
-
+        pred_object = out[..., 4]
+        pred_class = out[..., 5:]
         prediction = torch.cat((pred_bboxes.view(batch_size, -1, 4),
                                 pred_object.view(batch_size, -1, 1),
                                 pred_class.view(batch_size, -1, self.num_classes)),
@@ -62,33 +52,30 @@ class DetectLayer(nn.Module):
 
 
 class YOLOv3_head(nn.Module):
-    def __init__(self, input_size, num_classes, anchors):
+    def __init__(self, input_size, num_classes, anchors, num_anchor_per_scale):
         super().__init__()
         self.input_size = input_size
         self.num_classes = num_classes
-        self.num_anchor_per_scale = len(anchors[2])
-        self.anchor_L = torch.Tensor(anchors[2])
-        self.anchor_M = torch.Tensor(anchors[1])
-        self.anchor_S = torch.Tensor(anchors[0])
-        
+        self.anchor_scale = self.input_size / 416
+        self.num_anchor_per_scale = num_anchor_per_scale
+        self.anchor_L = torch.Tensor(anchors[2]) * self.anchor_scale
+        self.anchor_M = torch.Tensor(anchors[1]) * self.anchor_scale
+        self.anchor_S = torch.Tensor(anchors[0]) * self.anchor_scale
+
         self.head_L = DetectLayer(input_size=self.input_size,
-                                  in_channels=512,
                                   num_classes=self.num_classes,
                                   anchors=self.anchor_L,
                                   num_anchor_per_scale=self.num_anchor_per_scale)
 
         self.head_M = DetectLayer(input_size=self.input_size,
-                                  in_channels=256,
                                   num_classes=self.num_classes,
                                   anchors=self.anchor_M,
                                   num_anchor_per_scale=self.num_anchor_per_scale)
 
         self.head_S = DetectLayer(input_size=self.input_size,
-                                  in_channels=128,
                                   num_classes=self.num_classes,
                                   anchors=self.anchor_S,
                                   num_anchor_per_scale=self.num_anchor_per_scale)
-        
 
     def forward(self, x):
         P3, P4, P5 = x
@@ -118,9 +105,13 @@ if __name__ == "__main__":
     device = torch.device('cpu')
 
     x = torch.randn(1, 3, 416, 416).to(device)
+    num_classes = 80
+    num_attribute =  5 + num_classes
+    num_anchor_per_scale = 3
+    last_dim_channels = num_attribute * num_anchor_per_scale
     backbone = Darknet53_backbone(pretrained=True).to(device)
-    fpn = YOLOv3_FPN().to(device)
-    head = YOLOv3_head(input_size=input_size, num_classes=80, anchors=anchors)
+    fpn = YOLOv3_FPN(last_dim_channels=last_dim_channels).to(device)
+    head = YOLOv3_head(input_size=input_size, num_classes=80, anchors=anchors, num_anchor_per_scale=num_anchor_per_scale)
 
     with torch.no_grad():
         features = backbone(x)

models/yolov3_modules/neck.py
@@ -26,7 +26,7 @@ class TopDownLayer(nn.Module):
 
 
 class YOLOv3_FPN(nn.Module):
-    def __init__(self):
+    def __init__(self, last_dim_channels):
         super().__init__()
         self.topdown_1 = TopDownLayer(1024, 512)
         self.topdown_2 = TopDownLayer(768, 256)
@@ -34,15 +34,30 @@ class YOLOv3_FPN(nn.Module):
         self.lateral_1 = ConvLayer(512, 256, 1, stride=1, padding=0)
         self.lateral_2 = ConvLayer(256, 128, 1, stride=1, padding=0)
         self.upsample = nn.Upsample(scale_factor=2)
+        self.conv_S = nn.Sequential(
+            ConvLayer(128, 128*2, 3, stride=1, padding=1),
+            nn.Conv2d(128*2, last_dim_channels, 1, stride=1, padding=0)
+        )
+        self.conv_M = nn.Sequential(
+            ConvLayer(256, 256*2, 3, stride=1, padding=1),
+            nn.Conv2d(256*2, last_dim_channels, 1, stride=1, padding=0)
+        )
+        self.conv_L = nn.Sequential(
+            ConvLayer(512, 512*2, 3, stride=1, padding=1),
+            nn.Conv2d(512*2, last_dim_channels, 1, stride=1, padding=0)
+        )
     
     def forward(self, x):
         C3, C4, C5 = x
         P5 = self.topdown_1(C5)
+        ftr_S = self.conv_L(P5)
         C4 = torch.cat((self.upsample(P5), self.lateral_1(C4)), dim=1)
         P4 = self.topdown_2(C4)
+        ftr_M = self.conv_M(P4)
         C3 = torch.cat((self.upsample(P4), self.lateral_2(C3)), dim=1)
         P3 = self.topdown_3(C3)
-        return [P3, P4, P5]
+        ftr_L = self.conv_S(P3)
+        return [ftr_S, ftr_M, ftr_L]
 
 
 
@@ -50,8 +65,12 @@ if __name__ == "__main__":
     from backbone import Darknet53_backbone
     
     x = torch.randn(1, 3, 416, 416)
+    num_classes = 80
+    num_attribute =  5 + num_classes
+    num_anchor_per_scale = 3
+    last_dim_channels = num_attribute * num_anchor_per_scale
     backbone = Darknet53_backbone(pretrained=True)
-    fpn_module = YOLOv3_FPN()
+    fpn_module = YOLOv3_FPN(last_dim_channels)
 
     with torch.no_grad():
         features = backbone(x)

train.py
@@ -53,7 +53,7 @@ def execute_train(rank, dataloader, model, criterion, optimizer, class_list, col
     model.train()
 
     for index, mini_batch in enumerate(dataloader):
-        if (rank == 0) and (index == 0):
+        if index == 0:
             canvas_img = mini_batch[0][0]
             canvas_gt = mini_batch[1][0]
 
@@ -76,11 +76,11 @@ def execute_train(rank, dataloader, model, criterion, optimizer, class_list, col
             dataloader.set_postfix_str(s=f'{monitor_text}')
 
     total_loss /= len(dataloader)
+
     if OS_SYSTEM == 'Linux':
         dist.all_reduce(total_loss, op=dist.ReduceOp.SUM)
 
     canvas = visualize_target(canvas_img, canvas_gt, class_list, color_list) if rank == 0 else None
-
     del images, predictions, losses
     torch.cuda.empty_cache()
     return total_loss, canvas
@@ -106,6 +106,7 @@ def execute_val(rank, world_size, config, dataloader, model, criterion, class_li
         predictions = model(images)
         losses = criterion(predictions, targets)
         predictions = torch.cat(predictions, dim=1)
+        predictions[..., 4:] = torch.sigmoid(predictions[..., 4:])
 
         for idx in range(len(filenames)):
             filename = filenames[idx]
@@ -127,6 +128,7 @@ def execute_val(rank, world_size, config, dataloader, model, criterion, class_li
             dataloader.set_postfix_str(s=f'{monitor_text}')
 
     total_loss /= len(dataloader)
+
     if OS_SYSTEM == 'Linux':
         dist.all_reduce(total_loss, op=dist.ReduceOp.SUM)
         dist.all_gather_object(gather_objects, detections)
@@ -134,8 +136,7 @@ def execute_val(rank, world_size, config, dataloader, model, criterion, class_li
         gather_objects = [detections]
 
     min_conf_thresh = 0.1 # just for checking val result
-    canvas = visualize_prediction(canvas_img, detections[0], min_conf_thresh, class_list, color_list) if rank == 0 else None
-
+    canvas = visualize_prediction(canvas_img, detections[0][1], min_conf_thresh, class_list, color_list) if rank == 0 else None
     del images, predictions, losses
     torch.cuda.empty_cache()
     return total_loss, gather_objects, canvas
@@ -167,8 +168,7 @@ def main_work(rank, world_size, args, logger):
 
     if rank == 0:
         logging.warning(f'{train_set.data_info}')
-        logging.warning(f'{val_set.data_info}')
-        print(f'{train_set.data_info}| {val_set.data_info}')
+        logging.warning(f'{val_set.data_info}\n')
 
     model = YOLOv3_Model(config_path=args.config_path, num_classes=len(class_list))
     criterion = YOLOv3_Loss(config_path=args.config_path, model=model)
@@ -207,7 +207,7 @@ def main_work(rank, world_size, args, logger):
             dist.all_reduce(total_n_target, op=dist.ReduceOp.SUM)
 
         if rank == 0:
-            message = f'Best Possible Rate: {total_n_train/total_n_target:0.4f}, Train_target/Total_target: {total_n_train}/{total_n_target}\n'
+            message = f'Best Possible Rate: {total_n_train/total_n_target:.4f}, Train_target/Total_target: {total_n_train}/{total_n_target}\n'
             logging.warning(message)
 
         del dataloader
@@ -246,8 +246,9 @@ def main_work(rank, world_size, args, logger):
             val_loader = tqdm(val_loader, desc='[Phase:VAL]', ncols=110, leave=False)
 
         train_sampler.set_epoch(epoch)
-        train_loss, canvas_train = execute_train(rank=rank, dataloader=train_loader, model=model, criterion=criterion,
-                                                 optimizer=optimizer, class_list=class_list, color_list=color_list)
+        train_loss, canvas_train = execute_train(rank=rank, dataloader=train_loader, model=model,
+                                                 criterion=criterion, optimizer=optimizer,
+                                                 class_list=class_list, color_list=color_list)
         val_loss, gather_objects, canvas_val = execute_val(rank=rank, world_size=world_size, config=config_item,
                                                            dataloader=val_loader, model=model, criterion=criterion,
                                                            class_list=class_list, color_list=color_list)
@@ -265,25 +266,28 @@ def main_work(rank, world_size, args, logger):
             logging.warning(message + f' mAP Computation Time(sec): {time.time() - start:.4f}')
             logging.warning(eval_text)
 
-            if epoch % args.image_log_interval == 0:
-                imwrite(str(args.image_log_dir / 'train' /f'EP{epoch:03d}.jpg'), canvas_train)
-                imwrite(str(args.image_log_dir / 'val' /f'EP{epoch:03d}.jpg'), canvas_val)
+            if epoch % args.img_interval == 0:
+                imwrite(str(args.image_log_dir / 'train' / f'EP{epoch:03d}.jpg'), canvas_train)
+                imwrite(str(args.image_log_dir / 'val' / f'EP{epoch:03d}.jpg'), canvas_val)
 
-            if (epoch >= args.start_save) and (mAP_info['all']['mAP_50'] > best_mAP):
-                best_mAP = mAP_info['all']['mAP_50']
-                model_to_save = model.module if hasattr(model, 'module') else model
-                save_model(model = deepcopy(model_to_save).cpu(), save_path = args.weight_dir / f'weight_EP{epoch:03d}.pth')
+            if epoch >= args.start_save:
+                if mAP_info['all']['mAP_50'] > best_mAP:
+                    best_mAP = mAP_info['all']['mAP_50']
+                    model_to_save = model.module if hasattr(model, 'module') else model
+                    save_model(model=deepcopy(model_to_save).cpu(), save_path=args.weight_dir / f'weight_EP{epoch:03d}.pth')
 
-                analysis_result = analyse_mAP_info(mAP_info['all'], class_list)
-                data_df, figure_AP, figure_dets, fig_PR_curves = analysis_result
-                data_df.to_csv(str(args.analysis_log_dir / f'dataframe_EP{epoch:03d}.csv'))
-                figure_AP.savefig(str(args.analysis_log_dir / f'figure-AP_EP{epoch:03d}.png'))
-                figure_dets.savefig(str(args.analysis_log_dir / f'figure-dets_EP{epoch:03d}.png'))
+                    analysis_result = analyse_mAP_info(mAP_info['all'], class_list)
+                    data_df, figure_AP, figure_dets, fig_PR_curves = analysis_result
+                    data_df.to_csv(str(args.analysis_log_dir / f'dataframe_EP{epoch:03d}.csv'))
+                    figure_AP.savefig(str(args.analysis_log_dir / f'figure-AP_EP{epoch:03d}.png'))
+                    figure_dets.savefig(str(args.analysis_log_dir / f'figure-dets_EP{epoch:03d}.png'))
 
-                PR_curve_dir = args.analysis_log_dir / 'PR_curve' / f'EP{epoch:03d}'
-                os.makedirs(PR_curve_dir, exist_ok=True)
-                for class_id in fig_PR_curves.keys():
-                    fig_PR_curves[class_id].savefig(str(PR_curve_dir / f'{class_list[class_id]}.png'))
+                    PR_curve_dir = args.analysis_log_dir / 'PR_curve' / f'EP{epoch:03d}'
+                    os.makedirs(PR_curve_dir, exist_ok=True)
+                    for class_id in fig_PR_curves.keys():
+                        fig_PR_curves[class_id].savefig(str(PR_curve_dir / f'{class_list[class_id]}.png'))
+    if rank == 0:
+        logging.warning(f' Best mAP@0.5: {best_mAP:.3f}')
     cleanup()
 
 
@@ -293,8 +297,8 @@ def main():
     parser.add_argument('--config_path', type=str, default='config/yolov3.yml', help='Path to config.yml file')
     parser.add_argument('--exp_name', type=str, default=str(TIMESTAMP), help='Name to log training')
     parser.add_argument('--gpu_ids', type=int, default=[0], nargs='+', help='List of GPU IDs')
-    parser.add_argument('--image_log_interval', type=int, default=3, help='Image logging interval')
-    parser.add_argument('--start_save', type=int, default=10, help='Starting model saving epoch')
+    parser.add_argument('--img_interval', type=int, default=5, help='Image logging interval')
+    parser.add_argument('--start_save', type=int, default=1, help='Starting model saving epoch')
     parser.add_argument('--init_score', type=float, default=0.1, help='Initial mAP score for update best model')
 
     args = parser.parse_args()

utils/process.py
@@ -24,7 +24,7 @@ def run_NMS_for_YOLO(prediction, iou_threshold=0.5, maxDets=100):
     x2 = bboxes[:, 0] + bboxes[:, 2]/2
     y2 = bboxes[:, 1] + bboxes[:, 3]/2
     areas = (x2 - x1 + 1) * (y2 - y1 + 1)
-    order = scores.argsort()[::-1][:maxDets]
+    order = scores.argsort()[::-1]
             
     pick = []
     while len(order) > 0:
@@ -44,4 +44,4 @@ def run_NMS_for_YOLO(prediction, iou_threshold=0.5, maxDets=100):
         overlap = (w * h)
         ious = overlap / (areas[i] + areas[order[1:]] - overlap + 1e-8)
         order = order[np.where(ious <= iou_threshold)[0] + 1]
-    return prediction[pick]
+    return prediction[pick[:maxDets]]